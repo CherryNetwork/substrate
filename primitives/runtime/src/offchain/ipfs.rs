4d074087e4af7d82af017ead04cca731f46a7489
Charalampos Mitrodimas <charmitro@pm.me>
2022-09-06 16:02:16 +0300

    add(offchain/ipfs): comments

--- primitives/runtime/src/offchain/ipfs.rs
+++ primitives/runtime/src/offchain/ipfs.rs
@@ -1,3 +1,19 @@
+// This file is part of Cherry Networks.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! A high-level helpers for making IPFS requests from within Offchain Workers.
+
 use crate::offchain::http;
 use sp_core::offchain::Duration;
 use sp_io::offchain::timestamp;
@@ -6,6 +22,7 @@ use serde_json;
 use serde::{Serialize, Deserialize};
 
 
+/// /stats/repo response
 #[derive(Serialize, Deserialize, Debug)]
 pub struct RepoStatsResponse {
     #[serde(alias = "NumObjects")]
@@ -29,6 +46,7 @@ pub struct RepoStatsResponse {
 	"RepoSize": Number(4556418),
 	"StorageMax": Number(10000000000), "Version": String("fs-repo@12")
 */
+/// Get local IPFS repository stats
 pub fn repo_stats() -> RepoStatsResponse {
 	let request =
 		http::Request::get("http://127.0.0.1:5001/api/v0/stats/repo").method(http::Method::Post);
